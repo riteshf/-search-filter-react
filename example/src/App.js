a848ea4ed2a6cae5f2f12f47b38e91afa79ac103
Ritesh Firodiya <ritesh@Riteshs-MacBook-Pro.local>
2022-01-11 13:53:00 +0530

    dependencies resolved

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-import { ExampleComponent } from '@search-filter/react'
+import { SearchFilter } from '@search-filter/react'
 import '@search-filter/react/dist/index.css'
-
+import response from './__data/Shops_attributes_response.json'
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const updateFilter = (newFilter) => {
+    console.log(newFilter)
+  }
+  return (
+    <SearchFilter
+      options={response.response.data}
+      selected={{}}
+      updateFilter={updateFilter}
+    />
+  )
 }
 
 export default App
